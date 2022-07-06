ae75c127fd8b4a807b4ac5c66a57c4d7c81474ba
CGJI58 <leemsk22@naver.com>
2022-07-06 14:04:51 +0900

    too slow issue 질문을 위한 커밋

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
+
+browser.get("https://google.com")
+
+search_bar = browser.find_element_by_class_name("gLFyf")
+
+print(search_bar)
+
+browser.quit()
